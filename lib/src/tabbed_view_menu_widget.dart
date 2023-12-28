43bf1b0b1d805326d7231d02fc10fd3d021594f6
陈磊 <200892907@qq.com>
2023-12-28 14:18:55 +0800

    feat:修改tabbed_view_menu的问题

--- lib/src/tabbed_view_menu_widget.dart
+++ lib/src/tabbed_view_menu_widget.dart
@@ -20,7 +20,8 @@ class _TabbedViewMenuWidgetState extends State<TabbedViewMenuWidget> {
   Widget build(BuildContext context) {
     TabbedViewThemeData theme = TabbedViewTheme.of(context);
     TabbedViewMenuThemeData menuTheme = theme.menu;
-    bool hasDivider = menuTheme.dividerThickness > 0 && menuTheme.dividerColor != null;
+    bool hasDivider =
+        menuTheme.dividerThickness > 0 && menuTheme.dividerColor != null;
     int itemCount = widget.provider.menuItems.length;
     if (hasDivider) {
       itemCount += widget.provider.menuItems.length - 1;
@@ -33,14 +34,18 @@ class _TabbedViewMenuWidgetState extends State<TabbedViewMenuWidget> {
         if (hasDivider) {
           itemIndex = index ~/ 2;
           if (index.isOdd) {
-            return Divider(height: menuTheme.dividerThickness, color: menuTheme.dividerColor, thickness: menuTheme.dividerThickness);
+            return Divider(
+                height: menuTheme.dividerThickness,
+                color: menuTheme.dividerColor,
+                thickness: menuTheme.dividerThickness);
           }
         }
         return InkWell(
           hoverColor: menuTheme.hoverColor,
           onTap: () {
             widget.provider.menuItemsUpdater([]);
-            Function? onSelection = widget.provider.menuItems[itemIndex].onSelection;
+            Function? onSelection =
+                widget.provider.menuItems[itemIndex].onSelection;
             if (onSelection != null) {
               onSelection();
             }
@@ -49,22 +54,29 @@ class _TabbedViewMenuWidgetState extends State<TabbedViewMenuWidget> {
             padding: menuTheme.menuItemPadding,
             child: Text(
               widget.provider.menuItems[itemIndex].text,
-              overflow: menuTheme.ellipsisOverflowText ? TextOverflow.ellipsis : null,
+              overflow:
+                  menuTheme.ellipsisOverflowText ? TextOverflow.ellipsis : null,
             ),
           ),
         );
       },
     );
 
-    return Container(
-      margin: menuTheme.margin,
-      padding: menuTheme.padding,
-      decoration: menuTheme.decoration,
-      child: Material(
-        type: MaterialType.transparency,
-        textStyle: menuTheme.textStyle,
-        child: list,
-      ),
+    return Column(
+      children: [
+        Flexible(
+          child: Container(
+            margin: menuTheme.margin,
+            padding: menuTheme.padding,
+            decoration: menuTheme.decoration,
+            child: Material(
+              type: MaterialType.transparency,
+              textStyle: menuTheme.textStyle,
+              child: list,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
